cd7827904d8f68895738c3fad45b9709a6241394
leem919 <leem919@gmail.com>
2022-10-04 16:20:31 -0500

    Create msrbot_install.sh

---
+++ msrbot_install.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt install git pip chromium-driver -y
+pip3 install certifi chardet idna requests selenium urllib3 ipapi func_timeout random-word==1.0.7 pyyaml
+git clone https://github.com/farshadz1997/Microsoft-Rewards-bot
+echo DONE
