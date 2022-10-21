af14dd988b76f8097ce65122b541a29f270670aa
leem919 <leem919@gmail.com>
2022-10-21 13:45:25 -0700

    Update msrbot_install.sh

--- msrbot_install.sh
+++ msrbot_install.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install git pip wget chromium-driver -y
+sudo apt install git python3 pip wget chromium-driver -y
 pip3 install certifi chardet idna requests selenium urllib3 ipapi func_timeout random-word==1.0.7 pyyaml
 git clone https://github.com/farshadz1997/Microsoft-Rewards-bot
 cd Microsoft-Rewards-bot
