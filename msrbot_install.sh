b5cbf23ad76a904f12a7abc39d33c00e05946d46
leem919 <leem919@gmail.com>
2022-10-21 11:37:58 -0700

    Update msrbot_install.sh

--- msrbot_install.sh
+++ msrbot_install.sh
@@ -3,7 +3,6 @@ sudo apt install git pip wget chromium-driver -y
 pip3 install certifi chardet idna requests selenium urllib3 ipapi func_timeout random-word==1.0.7 pyyaml
 git clone https://github.com/farshadz1997/Microsoft-Rewards-bot
 cd Microsoft-Rewards-bot
-echo >> update.sh "rm ms_rewards_farmer.py"
-echo >> update.sh "wget https://raw.githubusercontent.com/farshadz1997/Microsoft-Rewards-bot/master/ms_rewards_farmer.py"
+wget https://raw.githubusercontent.com/leem919/ms-rewards-farmer-installer/main/update.sh
 chmod +x update.sh
 echo DONE
