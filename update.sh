183fd6553ba1785055defbf67929c1b55b1d6a62
leem919 <leem919@gmail.com>
2023-02-18 19:05:02 -0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,2 +1,6 @@
 rm ms_rewards_farmer.py
+rm requirements.txt
+wget https://raw.githubusercontent.com/farshadz1997/Microsoft-Rewards-bot/master/requirements.txt
+pip3 install -r requirements.txt
 wget https://raw.githubusercontent.com/farshadz1997/Microsoft-Rewards-bot/master/ms_rewards_farmer.py
+echo UPDATED
