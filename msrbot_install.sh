3b15abb0de2fd89555bd603c3697f29fcd65cdf8
leem919 <leem919@gmail.com>
2023-02-18 19:05:08 -0800

    Update msrbot_install.sh

--- msrbot_install.sh
+++ msrbot_install.sh
@@ -2,7 +2,7 @@ sudo apt update
 sudo apt install git python3 pip wget chromium-driver -y
 git clone https://github.com/farshadz1997/Microsoft-Rewards-bot
 cd Microsoft-Rewards-bot
-pip3 install -r requirements
+pip3 install -r requirements.txt
 wget https://raw.githubusercontent.com/leem919/ms-rewards-farmer-installer/main/update.sh
 chmod +x update.sh
 echo DONE
