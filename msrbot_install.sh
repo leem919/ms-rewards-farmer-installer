7173e21dfd090d70ca823ac233b23d3a8d54e070
leem919 <leem919@gmail.com>
2023-10-03 23:32:37 -0700

    Update msrbot_install.sh

--- msrbot_install.sh
+++ msrbot_install.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install git python3 python3-tk pip wget chromium -y
+sudo apt install git python3 python3-tk pip chromium -y
 git clone https://github.com/charlesbel/Microsoft-Rewards-Farmer
 cd Microsoft-Rewards-Farmer
 pip3 install -r requirements.txt
