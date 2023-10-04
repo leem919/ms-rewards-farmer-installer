e0890593fc3e5bec815f8a9406221d62f14803bf
leem919 <leem919@gmail.com>
2023-10-03 19:30:21 -0700

    Change script to charlesbel's bot

--- msrbot_install.sh
+++ msrbot_install.sh
@@ -1,8 +1,5 @@
 sudo apt update
-sudo apt install git python3 python3-tk pip wget chromium-driver -y
-git clone https://github.com/farshadz1997/Microsoft-Rewards-bot
-cd Microsoft-Rewards-bot
+sudo apt install git python3 python3-tk pip wget -y
+git clone https://github.com/charlesbel/Microsoft-Rewards-Farmer
+cd Microsoft-Rewards-Farmer
 pip3 install -r requirements.txt
-wget https://raw.githubusercontent.com/leem919/ms-rewards-farmer-installer/main/update.sh
-chmod +x update.sh
-echo DONE
