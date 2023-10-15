4ee04c5054ed9e3c89762e3d3fce68efad52b4a5
leem919 <leem919@gmail.com>
2023-10-15 13:59:06 -0700

    Update msrbot_install.sh

--- msrbot_install.sh
+++ msrbot_install.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt install git python3 python3-tk pip chromium -y
 git clone https://github.com/klept0/MS-Rewards-Farmer
-cd Microsoft-Rewards-Farmer
+cd MS-Rewards-Farmer
 pip3 install -r requirements.txt
 pip3 install selenium selenium-wire undetected-chromedriver --upgrade
