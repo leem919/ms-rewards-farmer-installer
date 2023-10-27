f28978359aa1f523c06a08c1d08e009ee82dd630
leem919 <leem919@gmail.com>
2023-10-27 14:38:30 -0700

    Update msrbot_install.sh

--- msrbot_install.sh
+++ msrbot_install.sh
@@ -3,4 +3,3 @@ sudo apt install git python3 python3-tk pip chromium -y
 git clone https://github.com/klept0/MS-Rewards-Farmer
 cd MS-Rewards-Farmer
 pip3 install -r requirements.txt --break-system-packages
-pip3 install pandas
